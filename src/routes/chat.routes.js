98615b255d109417633efe57e6d8c640108fc085
Sebastián Núñez <sebasnunezfoca@gmail.com>
2026-05-25 16:56:40 -0500

    fix: rutas middleware en todo el proyecto

--- src/routes/chat.routes.js
+++ src/routes/chat.routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const auth = require("../middlewares/auth.middleware");
+const auth = require("../middleware/auth");
 const { sendMessage, getMessages } = require("../controllers/chat.controller");
 
 router.post("/", auth, sendMessage);
