510792ebae77baf643ab882616541a186c195b88
Sebastián Núñez <sebasnunezfoca@gmail.com>
2026-05-25 16:52:59 -0500

    fix: ruta middleware auth

--- src/routes/report.routes.js
+++ src/routes/report.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const auth = require("../middlewares/auth");
+const auth = require("../middleware/auth");
 const { createReport } = require("../controllers/report.controller");
 
 router.post("/", auth, createReport);
